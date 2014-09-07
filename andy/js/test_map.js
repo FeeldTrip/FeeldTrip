bc6432441535cf5437fca3744b57c6dfc90469c4
Andreas Ettinger <ucsf@Skylla.local>
2014-09-07 04:37:15 -0700

    map try to center destination

--- andy/js/test_map.js
+++ andy/js/test_map.js
@@ -16,15 +16,9 @@ require([
    // extent: new Extent({"xmin":-2460944,"ymin":-1389910,"xmax":2297115,"ymax":1643787,"spatialReference":{"wkid":102100}}),
     basemap: "gray",
     center: [ 
-    {% if longitude %}
-    {{ longitude }}
-    {% else %}
     -118.4, 
-    {% if latitude %}
-    {{ latitude }} 
-    {% else %}
     34.08
-    {% endif %}],
+    ],
     zoom: 5,
    // maxScale: 5000000,
    // minScale: 20000000
