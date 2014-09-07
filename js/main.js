f412b669f467b0a75903da7f3f87882c265208e2
Jake Douglas <jbrandondouglas@gmail.com>
2014-09-07 01:55:10 -0700

    more js

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ require([
    // extent: new Extent({"xmin":-2460944,"ymin":-1389910,"xmax":2297115,"ymax":1643787,"spatialReference":{"wkid":102100}}),
     basemap: "gray",
     center: [-118.4, 34.08],
-    zoom: 1,
+    zoom: 4,
    // maxScale: 5000000,
    // minScale: 20000000
   });
