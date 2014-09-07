46572ad9180975be9df31e44d527f779dcc40291
Jake Douglas <jbrandondouglas@gmail.com>
2014-09-06 20:53:35 -0700

    update

---
+++ js/main.js
@@ -0,0 +1,18 @@
+take sentiment data (sentNumber)
+take geolocation data (long, lat)
+
+add circle to map at location
+modify circle to reflect sentNumber
+
+
+for each location(long,lat)
+	var sentNumber = sentNumber
+	add circle
+	 circleCSS (size, color)
+
+function showData () {
+	
+}
+
+var sentNumber = X
+
