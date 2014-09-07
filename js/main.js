12bc4614c5f7e5c210689bcef6e371cac07d586d
Jake Douglas <jbrandondouglas@gmail.com>
2014-09-06 22:42:15 -0700

    lady coded everything for us

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
-take sentiment data (sentNumber)
-take geolocation data (long, lat)
+// // take sentiment data (sentNumber)
+// // take geolocation data (long, lat)
 
-add circle to map at location
-modify circle to reflect sentNumber
+// // add circle to map at location
+// // modify circle to reflect sentNumber
 
 
-for each location(long,lat)
-	var sentNumber = sentNumber
-	add circle
-	 circleCSS (size, color)
+// for each location(long,lat)
+// 	var sentNumber = sentNumber
+// 	add circle
+// 	 circleCSS (size, color)
 
-function showData () {
-	
-}
+$( "#greenCircle" ).hover(
+	function() {
+  	$( ".data" ).fadeIn( 100 );
+  }, function() {
+  $( ".data" ).fadeOut( 100 );
+});
 
-var sentNumber = X
+// var sentNumber = X
+
+
 
