66d25d91e9089f246bff50f9cab52e4193d8b3cc
Jake Douglas <jbrandondouglas@gmail.com>
2014-09-07 06:25:06 -0700

    25

--- js/main.js
+++ js/main.js
@@ -71,10 +71,10 @@ require([
         
         graphicsLayer.add(graphic);
 
-        var pointNY = new Point(-41,74);
+        var pointNY = new Point(-74,41);
       
         var graphic = new Graphic(point);
-        graphic.setAttributes({"XCoord":-41,"YCoord":74,"Sentiment":"Happy", "Factor": 18});
+        graphic.setAttributes({"XCoord":-74,"YCoord":41,"Sentiment":"Happy", "Factor": 18});
         
         graphicsLayer.add(graphic);
 
